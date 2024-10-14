e1b3cdb64ff6c3d136df1c82c7776474e38fb350
LoRd123545 <kamilabbasi694@gmail.com>
2024-10-14 09:04:33 +0200

    removed unnecessary @ts-check

--- express-ts/eslint.config.js
+++ express-ts/eslint.config.js
@@ -1,10 +1,7 @@
-// @ts-check
-
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
-
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
@@ -12,4 +9,4 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   prettierConfig,
-];
+]
