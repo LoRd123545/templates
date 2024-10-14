6d847afd2ee1badd5e73cacb26d39a20746fd486
LoRd123545 <kamilabbasi694@gmail.com>
2024-10-14 09:03:31 +0200

    this commit adds template for model

---
+++ express-ts/src/models/example.model.ts
@@ -0,0 +1,13 @@
+const find = async () => {};
+const findOne = async () => {};
+const create = async () => {};
+const update = async () => {};
+const remove = async () => {};
+
+export const exampleModel = {
+  find,
+  findOne,
+  create,
+  update,
+  remove,
+};
