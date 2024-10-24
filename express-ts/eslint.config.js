8c3a9fa29703ae2cba8eaea9f91bf1a3af935bfb
LoRd123545 <kamilabbasi694@gmail.com>
2024-10-24 20:33:58 +0200

    dfdf

--- express-ts/eslint.config.js
+++ express-ts/eslint.config.js
@@ -1,12 +1,15 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import prettierConfig from "eslint-config-prettier";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  prettierConfig,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn'
+    },
+  },
 ]
--- express-ts/src/index.ts
+++ express-ts/src/index.ts
@@ -1,22 +1,22 @@
-import { createServer } from 'http';
+import { createServer } from 'http'
 
-import express from 'express';
+import express from 'express'
 
-import { env } from './env.js';
+import { env } from './env.js'
 
-import appRouter from '@app/routes/index.js';
+import appRouter from '@app/routes/index.js'
 
-const app = express();
-const server = createServer(app);
+const app = express()
+const server = createServer(app)
 
-app.use('/v1', appRouter);
+app.use('/v1', appRouter)
 
 server.listen(env.PORT, () => {
-  console.log(`http server listening on port ${env.PORT}`);
-});
+  console.log(`http server listening on port ${env.PORT}`)
+})
 
-const gracefulShutdown = () => {};
+const gracefulShutdown = () => {}
 
-process.on('SIGINT', gracefulShutdown);
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGUSR2', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown)
+process.on('SIGTERM', gracefulShutdown)
+process.on('SIGUSR2', gracefulShutdown)
